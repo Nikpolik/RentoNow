d4a16e3c7018b0cc2b0e22afc73d2d76f90708a7
nikpolik <n.a.polikandriotis@gmail.com>
2020-11-22 12:29:14 +0200

    Initialize spring project

---
+++ src/main/java/gr/athtech/groupName/rentonow/RentonowApplication.java
@@ -0,0 +1,13 @@
+package gr.athtech.groupName.rentonow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentonowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentonowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/athtech/groupName/rentonow/RentonowApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.athtech.groupName.rentonow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentonowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
