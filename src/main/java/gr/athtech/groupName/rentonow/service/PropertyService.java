afa06b177fee87f4d0054359e86a88b4ead4bd33
Kiriweb <44785406+Kiriweb@users.noreply.github.com>
2020-12-13 16:21:25 +0200

    Services

--- src/main/java/gr/athtech/groupName/rentonow/service/PropertyService.java
+++ src/main/java/gr/athtech/groupName/rentonow/service/PropertyService.java
@@ -3,6 +3,7 @@ package gr.athtech.groupName.rentonow.service;
 import gr.athtech.groupName.rentonow.model.Property;
 
 public interface PropertyService {
+
     Property getById(long id);
     List<Property> getAllProperties();
 
