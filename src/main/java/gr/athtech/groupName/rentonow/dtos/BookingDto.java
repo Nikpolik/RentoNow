493713e9e7eb99c0791ec1092aa7e07f01185051
nikpolik <n.a.polikandriotis@gmail.com>
2021-01-08 13:55:38 +0200

    Get dates of booking from availability for BookingDTO

--- src/main/java/gr/athtech/groupName/rentonow/dtos/BookingDto.java
+++ src/main/java/gr/athtech/groupName/rentonow/dtos/BookingDto.java
@@ -13,20 +13,11 @@ public class BookingDto {
     private UserDto guest;
     private Long property;
 
-    public static Booking toBooking(BookingDto bookingDto) {
-        Booking booking = new Booking();
-        booking.setId(bookingDto.getId());
-        booking.setStartDate(bookingDto.getStartDate());
-        booking.setEndDate(bookingDto.getEndDate());
-        booking.setGuest(UserDto.toUser(bookingDto.getGuest()));
-        return booking;
-    }
-
     public static BookingDto fromBooking(Booking booking) {
         BookingDto bookingDto = new BookingDto();
         bookingDto.setId(booking.getId());
-        bookingDto.setStartDate(booking.getStartDate());
-        bookingDto.setEndDate(booking.getEndDate());
+        bookingDto.setStartDate(booking.getAvailability().getStartDate());
+        bookingDto.setEndDate(booking.getAvailability().getEndDate());
         bookingDto.setGuest(UserDto.fromUser(booking.getGuest()));
         bookingDto.setProperty(booking.getProperty().getId());
         return bookingDto;
