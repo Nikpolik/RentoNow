c9068a5e812e2f8f6253f382f6e9709c771f1d17
nikpolik <n.a.polikandriotis@gmail.com>
2021-01-08 17:50:40 +0200

    Add private constructor to const

--- src/main/java/gr/athtech/groupName/rentonow/constants/SecurityConstants.java
+++ src/main/java/gr/athtech/groupName/rentonow/constants/SecurityConstants.java
@@ -1,6 +1,9 @@
 package gr.athtech.groupName.rentonow.constants;
 
 public class SecurityConstants {
+
+  private SecurityConstants() {}
+
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
   public static final String SIGN_UP_URL = "/auth/register";  
